e1eeb45e7a305da5124dbc8e7bc0922747454d76
Jake Fell <jakefell@hotmail.co.uk>
2017-06-21 11:52:31 +0100

    Finished chapter 7 for the SportsStore

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _repository = productRepository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = _repository.Products
+                    .OrderBy(p => p.ProductId)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _repository.Products.Count()
+                }
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernal)
+        {
+            _kernel = kernal;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            _kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
