9a88f3f4cf1d3071c9afa2b364704afe050a329d
Jake Fell <jakefell@hotmail.co.uk>
2017-06-27 16:43:39 +0100

    Finished up to Chapter 12

--- SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -19,5 +19,38 @@ namespace SportsStore.Domain.Concrete
                 return _context.Products;
             }
         }
+
+        public Product DeleteProduct(int productId)
+        {
+            Product dbEntry = _context.Products.Find(productId);
+            if (dbEntry != null)
+            {
+                _context.Products.Remove(dbEntry);
+                _context.SaveChanges();
+            }
+
+            return dbEntry;
+        }
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductId == 0)
+            {
+                _context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = _context.Products.Find(product.ProductId);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+
+            _context.SaveChanges();
+        }
     }
 }
---
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,64 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository _repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(_repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = _repository.Products.FirstOrDefault(p => p.ProductId == productId);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int productId)
+        {
+            Product deletedProduct = _repository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = string.Format("{0} was deleted", deletedProduct.Name);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
